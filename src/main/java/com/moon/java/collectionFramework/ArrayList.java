1b79205fde197776f4f4b8ec57b18e53a65d756b
Moon <rs.moony@gmail.com>
2022-04-11 10:39:31 +0900

    ## Array List Type Convert to Generic ### 2022-04-11 - made arrayList class by Pojo.

--- src/main/java/com/moon/java/collectionFramework/ArrayList.java
+++ src/main/java/com/moon/java/collectionFramework/ArrayList.java
@@ -5,79 +5,94 @@ public class ArrayList<T> {
     int capacity = 0;
     int size = 0;
 
-    ArrayList(int capacity){
+    public ArrayList(int capacity){
         this.capacity = capacity;
         arr = new Object[capacity];
         size = 0;
     }
 
-    ArrayList(){
+    public ArrayList(){
         capacity = 1;
         arr = new Object[capacity];
         size = 0;
     }
-    public void add(Object element){
-        if (size == capacity){
+
+    public void add(T element){
+        if(size == capacity){
             expandArray();
         }
-        arr[size++] =element;
-    }
-    public void addFirst(Object element){
-        add(0,element);
+
+        arr[size++] = element;
     }
 
-    private void add(int index, Object element) {
-        if (size == capacity){
+    public void addFirst(T element){
+        add(0, element);
+    }
+
+    public void add(int index, T element){
+        if(size == capacity){
             expandArray();
         }
-        for (int i = size; i >= index ; i--) {
-            arr[i]= arr[i-1];
 
-            arr[index] = element;
-            size ++;
-        }
+        for(int i = size; i >= index; i--)
+            arr[i] = arr[i-1];
+
+        arr[index] = element;
+        size++;
     }
 
-    private void expandArray() {
+    private void expandArray(){
         capacity *= 2;
-        Object [] tempArr = new Object[capacity];
+        Object[] tempArr = new Object[capacity];
         copyArr(tempArr, arr);
         arr = new Object[capacity];
         copyArr(arr, tempArr);
     }
+
     private void copyArr(Object[] arr1, Object[] arr2){
-        for (int i = 0; i < arr2.length; i++) {
-            arr[i] = arr2[i];
+        for(int i = 0; i < arr2.length; i++){
+            arr1[i] = arr2[i];
         }
     }
 
-    public Object get(int index){
+    public T get(int index){
         if(size <= 0){
-        System.out.println("배열이 비어 있습니다.");
-        return null;
+            System.out.println("배열이 비어있습니다.");
+            return null;
         }
-        return arr[index];
+
+        return (T) arr[index];
     }
 
-    public Object remove(int index){
+    public T remove(int index){
         /*
-        * 크기 초과, 이미 비어있는지 등 조건문은 원하는대로 추가해주면 된다.
-        * */
-        Object result = arr[index];
+            크기 초과, 이미 비어있는지 등 조건문은 원하는대로 추가해주면 된다.
+        */
+        T result = (T) arr[index];
         arr[index] = null;
+        size--;
+        if(size > 0) {
+        /*
+        	삭제한 이후부터 앞으로 한칸씩 땡긴다.
+        */
+            for(int i = index; i < size; i++) {
+                arr[i] = arr[i+1];
+            }
+        }
+
 
         return result;
     }
-
     public void reset(){
-        arr= new Object[capacity];
+        arr = new Object[capacity];
+        size = 0;
     }
 
-    public int size(){
+    public int size() {
         return this.size;
     }
 
-    public int getCapacity(){
+    public int getCapacity() {
         return this.capacity;
     }
 }
